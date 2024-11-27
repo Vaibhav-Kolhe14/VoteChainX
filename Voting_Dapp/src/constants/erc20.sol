b742f879f24622520c4442d2e00286e8cd9c912c
Vaibhav14 <135616354+Vaibhav-Kolhe14@users.noreply.github.com>
2024-11-27 19:53:30 +0530

    Update erc20.sol update

--- Voting_Dapp/src/constants/erc20.sol
+++ Voting_Dapp/src/constants/erc20.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CKToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("CKToken", "CK") {
+contract VKToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("VKToken", "VK") {
         _mint(msg.sender, initialSupply);
     }
 
 }
-
-//  ETH : 10000 & Wei : 10000000000000000000000
